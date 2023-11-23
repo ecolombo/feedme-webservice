994f6e6ba5a4199048fa804abcb1d81c03bb8afb
ecolombo <elia.colombo@gmail.com>
2023-11-23 16:04:14 +0100

    First commit: Initializr package

---
+++ src/main/java/com/edesign/feedme/FeedmeWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.edesign.feedme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedmeWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedmeWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edesign/feedme/FeedmeWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edesign.feedme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedmeWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
